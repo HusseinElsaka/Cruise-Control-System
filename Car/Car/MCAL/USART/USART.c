3b4d5b3cbdb7016534591ccf108c64b1f63ee88d
HassanZakhira <91142808+HassanZakhira@users.noreply.github.com>
2022-08-22 19:41:54 +0200

    Add files via upload

---
+++ Car/Car/MCAL/USART/USART.c
@@ -0,0 +1,50 @@
+/*
+ * USART.c
+ *
+ *  Created on: Aug 22, 2022
+ *      Author: PClaptop
+ */
+#include "USART.h"
+
+
+
+void UART_init()
+{
+	//Set baud rate using the UBRR register.
+	UBRRL = BAUD_PRESCALE;		/* Load lower 8-bits of the baud rate value */
+	UBRRH = (BAUD_PRESCALE >> 8);	/* Load upper 8-bits*/
+
+	//Enable transmission and reception by using the UCSRB register.
+	SET_BIT(UCSRB,TXEN);//TXEN
+	SET_BIT(UCSRB,RXEN);
+	//Set data bit size to 8 bit by using the UCSRC register.
+	SET_BIT(UCSRC,URSEL);
+	SET_BIT(UCSRC,UCSZ0);
+	SET_BIT(UCSRC,UCSZ1);
+
+
+
+}
+
+void UART_TxChar(u8_t ch)
+{
+	while (! (UCSRA & (1<<UDRE)));	/* Wait for empty transmit buffer*/
+	UDR = ch ;
+}
+
+u8_t UART_RxChar()
+{
+	while (!(UCSRA & (1 << RXC)));/* Wait till data is received */
+	return(UDR);		/* Return the byte */
+}
+
+void UART_SendString(char *str)
+{
+	unsigned char j=0;
+
+	while (str[j]!=0)		/* Send string till null */
+	{
+		UART_TxChar(str[j]);
+		j++;
+	}
+}
---
+++ Car/Car/MCAL/USART/USART.h
@@ -0,0 +1,24 @@
+/*
+ * USART.h
+ *
+ *  Created on: Aug 22, 2022
+ *      Author: PClaptop
+ */
+
+#ifndef MCAL_USART_USART_H_
+#define MCAL_USART_USART_H_
+
+#include "../../Utilites/BitMath.h"
+#include "../../Utilites/Memmap.h"
+#include "../../Utilites/Datatypes.h"
+
+#define F_CPU 8000000UL			// Define frequency here its 8MHz
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
+
+void UART_init();
+void UART_TxChar( u8_t ch);
+u8_t UART_RxChar();
+
+#endif /* MCAL_USART_USART_H_ */
