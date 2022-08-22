0b3082d9ffbc09d22b76f465d803d6f1caf86116
HusseinElsaka <ehussein366@gmail.com>
2022-08-22 03:02:14 +0200

    Application

---
+++ Car/Car/Application/Application.h
@@ -0,0 +1,21 @@
+
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+#define F_CPU 8000000UL
+
+#include "../HAL/LCD Driver/LCD.h"
+#include "../MCAL/ADC Driver/adc.h"
+
+
+
+Str_Timer0Configuration_t timer0app;
+
+
+void app_init(void);
+void app_start(void);
+
+
+
+#endif /* APPLICATION_H_ */
