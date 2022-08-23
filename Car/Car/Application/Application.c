23890759ecd1fe32fd73b0a4144d91209b07edc1
HusseinElsaka <ehussein366@gmail.com>
2022-08-23 16:09:30 +0200

    Test the LCD and UART

--- Car/Car/Application/Application.c
+++ Car/Car/Application/Application.c
@@ -1,15 +1,14 @@
 #include "Application.h"
 
-u8_t flag = 0;
 
- 
+
 
 
 void app_init(void)
 {
 	/* Initialization */
-	DIO_setPortDirection(PORT_A,0xFF);
 	Ultrasonic_init();
+	
 }
 
 
@@ -17,3 +16,4 @@ void app_start(void)
 {
 	Ultrasonic_getReading();
 }
+
