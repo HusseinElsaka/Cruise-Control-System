ed44987b74daedc5f6598505819c918cb5379c09
HusseinElsaka <ehussein366@gmail.com>
2022-08-24 22:35:24 +0200

    Update the application

--- Car/Car/Application/Application.h
+++ Car/Car/Application/Application.h
@@ -6,7 +6,18 @@
 
 
 #include "../HAL/Ultrasonic Driver/Ultrasonic.h"
+#include "../MCAL/Timers Drivers/Timer1 Driver/timer1.h"
+#include "../HAL/Button Driver/button.h"
 
+#define BRAIKE_MODE				 0
+#define CONTROL_INCREMENT_MODE	 1
+#define CONTROL_DECREMENT_MODE	 2
+#define CONTROL_ULTRA_SONIC_MODE 3
+
+extern u8_t current_mode;
+Str_Timer1Configuration_t Timer1App;
+
+void Button_check(void);
 
 
 void app_init(void);
